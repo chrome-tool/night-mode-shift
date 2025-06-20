f96a7778408db23bdbf0bb17df2be84525fd583e
RyuSeiri <56245152+RyuSeiri@users.noreply.github.com>
2025-06-20 11:18:06 +0900

    update

--- assets/js/background.js
+++ assets/js/background.js
@@ -7,18 +7,47 @@ class background {
     const darkModeVal = await getDarkMode();
     const colorval = await getColor();
     const opacityVal = await getOpacity();
-    await chrome.tabs.sendMessage(tabId, {
-      action: "execute",
-      params: { darkMode: darkModeVal, color: colorval, opacity: opacityVal },
+    await chrome.scripting.executeScript({
+      target: { tabId },
+      files: ["assets/js/content.js"],
     });
+
+    await new Promise((resolve) => setTimeout(resolve, 100));
+
+    try {
+      await chrome.tabs.sendMessage(tabId, {
+        action: "execute",
+        params: { darkMode: darkModeVal, color: colorval, opacity: opacityVal },
+      });
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
   }
 
-  init() {
-    chrome.tabs.onActivated.addListener((activeInfo) => {
-      this.execute(activeInfo.tabId);
+  async init() {
+    chrome.tabs.onActivated.addListener(async (activeInfo) => {
+      await chrome.tabs.get(activeInfo.tabId, (tab) => {
+        if (
+          !tab.url ||
+          tab?.url?.startsWith("chrome://") ||
+          tab?.url?.startsWith("chrome-extension://") ||
+          tab?.url?.startsWith("file://")
+        ) {
+          return;
+        }
+        this.execute(activeInfo.tabId);
+      });
     });
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      this.execute(tabId);
+    chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+      if (
+        !tab.url ||
+        tab?.url?.startsWith("chrome://") ||
+        tab?.url?.startsWith("chrome-extension://") ||
+        tab?.url?.startsWith("file://")
+      ) {
+        return;
+      }
+      await this.execute(tabId);
     });
   }
 }
